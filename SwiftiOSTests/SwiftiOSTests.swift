6b1a54360e6f907ce831315cd551fd8247af51d4
nemesit <nemesit@myriadvisuals.com>
2014-06-10 13:09:55 +0200

    Initial Commit

---
+++ SwiftiOSTests/SwiftiOSTests.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftiOSTests.swift
+//  SwiftiOSTests
+//
+//  Created by Felix Grabowski on 10/06/14.
+//  Copyright (c) 2014 Felix Grabowski. All rights reserved.
+//
+
+import XCTest
+
+class SwiftiOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
