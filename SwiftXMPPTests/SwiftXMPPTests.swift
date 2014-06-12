e0584f12e7100d6fb6d82f3c7b947fcb333a8763
nemesit <nemesit@myriadvisuals.com>
2014-06-12 15:58:14 +0200

    removed useless files
    Signed-off-by: nemesit <nemesit@myriadvisuals.com>

---
+++ SwiftXMPPTests/SwiftXMPPTests.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftXMPPTests.swift
+//  SwiftXMPPTests
+//
+//  Created by Felix Grabowski on 10/06/14.
+//  Copyright (c) 2014 Felix Grabowski. All rights reserved.
+//
+
+import XCTest
+
+class SwiftXMPPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+      self.measureBlock({
+        for i in 0..1000 {
+          println("i'm not so fast")
+        }
+        })
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
