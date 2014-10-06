125039da052b30678ff7ddbc801a3088c532a4b8
nemesit <nemesit@myriadvisuals.com>
2014-10-06 13:27:12 +0200

    hm seems i forgot to commit somthing xD

---
+++ SwiftXMPP/BuddyListViewController.swift
@@ -0,0 +1,131 @@
+//
+//  BuddyListViewController.swift
+//  SwiftXMPP
+//
+//  Created by Felix Grabowski on 10/06/14.
+//  Copyright (c) 2014 Felix Grabowski. All rights reserved.
+//
+
+import UIKit
+
+class BuddyListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ChatDelegate {
+
+  @IBOutlet var tView: UITableView
+  var onlineBuddies: NSMutableArray = []
+  
+//  init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+//    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//    // Custom initialization
+//  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tView.delegate = self
+    tView.dataSource = self
+    let delegate = appDelegate()
+    delegate.chatDelegate = self
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    var login : AnyObject! = NSUserDefaults.standardUserDefaults().objectForKey("userID")
+    if login {
+      if appDelegate().connect() {
+        //show buddy list
+      } else {
+        showLogin()
+      }
+    }
+  }
+  
+  func showLogin() {
+    let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+    let loginController : AnyObject! = storyBoard.instantiateViewControllerWithIdentifier("loginViewController")
+    presentViewController(loginController as UIViewController, animated: true, completion: nil)
+  }
+  
+  func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+    var s: NSString = onlineBuddies.objectAtIndex(indexPath.row) as NSString
+    let cellIdentifier = "UserCellIdentifier"
+    var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+    
+    if !cell {
+      
+      cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+//      println("cell : \(cell)")
+    }
+    
+    if let c = cell? {
+      c.textLabel.text = s
+      c.accessoryType = .DisclosureIndicator
+    }
+    
+    
+//    cell!.textLabel.text = s
+//    cell!.accessoryType = .DisclosureIndicator
+    return cell;
+  }
+  
+  func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+    return onlineBuddies.count
+  }
+  
+  func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+    return 1
+  }
+  
+  func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    println("didSelectRowAtIndexPath")
+//    var userName: String? = onlineBuddies.objectAtIndex(indexPath.row) as? String
+//    var storyBoard = UIStoryboard(name: "Main", bundle: nil)
+//    var chatController: ChatViewController? = storyBoard.instantiateViewControllerWithIdentifier("chatViewController") as? ChatViewController
+//    if let controller = chatController? {
+//      controller.chatWithUser = userName!
+//      presentModalViewController(controller, animated: true)
+//    }
+//    println(chatController)
+  }
+  
+  func newBuddyOnLine(buddyName: NSString) {
+    onlineBuddies.addObject(buddyName)
+//    println("new buddy online: \(buddyName)")
+    tView.reloadData()
+  }
+
+  func buddyWentOffline(buddyName: NSString) {
+//    onlineBuddies.removeObject(buddyName)
+//    tView.reloadData()
+  }
+
+  func appDelegate() -> AppDelegate {
+    return UIApplication.sharedApplication().delegate as AppDelegate
+  }
+  
+  func xmppStream () -> XMPPStream {
+    return appDelegate().xmppStream!
+  }
+  
+  func didDisconnect() {
+    onlineBuddies.removeAllObjects()
+    tView.reloadData()
+  }
+  
+  override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    if let controller: ChatViewController? = segue?.destinationViewController as? ChatViewController {
+      if let cell: UITableViewCell? = sender as? UITableViewCell {
+        var str = tView.indexPathForCell(cell).row
+        controller!.chatWithUser = onlineBuddies.objectAtIndex(str) as String
+      }
+    }
+    println("fuckthisshit \(sender)")
+    
+  }
+
+}
---
+++ SwiftXMPP/ChatViewController.swift
@@ -0,0 +1,160 @@
+//
+//  ChatViewController.swift
+//  SwiftXMPP
+//
+//  Created by Felix Grabowski on 10/06/14.
+//  Copyright (c) 2014 Felix Grabowski. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageDelegate {
+  
+  @IBOutlet var messageField: UITextField
+  @IBOutlet var container : UIView
+  @IBOutlet var bottomContainerConstraint : NSLayoutConstraint
+  var chatWithUser: NSString = ""
+  @IBOutlet var tView: UITableView
+  var messages: NSMutableArray = []
+  
+//  init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+//    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//    // Custom initialization
+//  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tView.dataSource = self
+    tView.delegate = self
+    //self.messageField.becomeFirstResponder()
+    var del = appDelegate()
+    del.messageDelegate = self
+    messageField.becomeFirstResponder()
+    
+      // Do any additional setup after loading the view.
+    
+    registerForKeyboardNotifications()
+    
+  }
+  
+  func registerForKeyboardNotifications () {
+    let nc = NSNotificationCenter.defaultCenter()
+    nc.addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+    nc.addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
+  }
+
+  func keyboardWasShown(aNotification: NSNotification) {
+    println("wohoo keyboards")
+    var constraint = bottomContainerConstraint
+    println("before: \(view.constraints())")
+    var info: NSDictionary = aNotification.userInfo
+    var kbSize : CGRect = info.objectForKey(UIKeyboardFrameBeginUserInfoKey).CGRectValue()
+    
+    var visualString = "V:[container]-\(kbSize.height)-|"
+    
+    var newConstraint: NSArray = NSLayoutConstraint.constraintsWithVisualFormat(visualString, options:nil, metrics: nil, views: ["container" : container])
+    
+    view.removeConstraint(constraint)
+    view.addConstraints(newConstraint)
+    
+    view.updateConstraints()
+    
+    println("\n after: \(view.constraints())")
+    println("old constraint: \(constraint)")
+    println("new constraint: \(newConstraint[0])")
+  }
+  
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+
+  @IBAction func sendMessage() {
+    var messageStr: String = messageField.text
+    println(messageStr)
+    if messageStr.lengthOfBytesUsingEncoding(NSUTF8StringEncoding) > 0 {
+      var body = DDXMLElement.elementWithName("body") as DDXMLElement
+      body.setStringValue(messageStr)
+      var message = DDXMLElement.elementWithName("message") as DDXMLElement
+      message.addAttributeWithName("type", stringValue: "chat")
+      message.addAttributeWithName("to", stringValue: chatWithUser)
+      message.addChild(body)
+      xmppStream().sendElement(message)
+      messageField.text = ""
+      
+      var m: NSMutableDictionary = [:]
+      m["msg"] = messageStr
+      m["sender"] = "you"
+//      println("m: \(m) and message: \(messageStr)")
+
+      messages.addObject(m)
+      tView.reloadData()
+
+    }
+  }
+  
+  func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+    var s = messages.objectAtIndex(indexPath.row) as NSDictionary
+    let cellIdentifier = "MessageCellIdentifier"
+    var cell:UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+    if !cell {
+      cell = UITableViewCell(style: .Value1, reuseIdentifier: cellIdentifier)
+    }
+    
+    if let c = cell? {
+//      println(s)
+      c.textLabel.text = s["msg"] as String
+      c.detailTextLabel.text = s["sender"] as String
+      c.accessoryType = .None
+      c.userInteractionEnabled = false
+    }
+    
+    return cell
+  }
+  
+  func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+    return messages.count
+  }
+  
+  func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+    return 1
+  }
+  
+  @IBAction func closeChat() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func appDelegate() -> AppDelegate {
+    return UIApplication.sharedApplication().delegate as AppDelegate
+  }
+  
+  func xmppStream () -> XMPPStream {
+    return appDelegate().xmppStream!
+  }
+  
+  func newMessageReceived(messageContent: NSDictionary) {
+    println("receivedMessage")
+    messages.addObject(messageContent)
+    tView.reloadData()
+    var topIndexPath = NSIndexPath(forRow: (messages.count - 1), inSection: 0)
+    tView.scrollToRowAtIndexPath(topIndexPath, atScrollPosition: .Middle, animated: true)
+  }
+
+  
+  
+  
+  
+  
+  
+  /*
+  // #pragma mark - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+      // Get the new view controller using [segue destinationViewController].
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
